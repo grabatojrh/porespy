f4e7f13e86a2a51400f8e5bca312fa2c9cb6cd36
grabatojrh <grabatojrh@gmail.com>
2025-05-19 01:19:31 +0200

    minor edits

--- blobs_practice.py
+++ blobs_practice.py
@@ -3,23 +3,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pyvista as pv
 
-im = ps.generators.blobs(shape=[200,200,200], porosity = 0.2, blobiness=0.5)
+# im = ps.generators.blobs(shape=[200,200,200], porosity = 0.2, blobiness=0.5)
+# fig, ax = plt.subplots(figsize=[10, 10])
+# ax.imshow(ps.visualization.show_3D(im), interpolation='none')
+# ax.axis(False);
+
+# Set the desired pore (sphere) radius
+pore_radius = 5  # Change this value to control pore size
+
+# Generate 3D image with tunable pore size
+im = ps.generators.overlapping_spheres(
+    shape=[200, 200, 200],
+    r=pore_radius,
+    porosity=0.2,
+    maxiter=0
+)
+# Visualize the generated image
 fig, ax = plt.subplots(figsize=[10, 10])
 ax.imshow(ps.visualization.show_3D(im), interpolation='none')
 ax.axis(False);
-
-# # Get the coordinates of the non-zero (True) voxels
-# x, y, z = np.where(im > 0)
-
-# # Plot the 3D scatter plot
-# fig = plt.figure(figsize=(10, 10))
-# ax = fig.add_subplot(111, projection='3d')
-# ax.scatter(x, y, z, c=z, cmap='viridis', marker='o', s=1, alpha=0.5)
-
-# # Set labels and title
-# ax.set_xlabel('X')
-# ax.set_ylabel('Y')
-# ax.set_zlabel('Z')
-# ax.set_title('3D Visualization of Blobs')
-
-# plt.show()
